ac1cde5cd95365db2985667a611e2a51a20bab64
rorikae ^.^ <164604844+rorikae-busy@users.noreply.github.com>
2026-04-06 09:59:25 +0800

    Update index.php
    may binago lang ako sa panglink papuntang todo

--- index.php
+++ index.php
@@ -14,12 +14,11 @@
         <span class="word1">daily</span> <span class="word2">planner...</span>
     </h1>
 
-    <!-- Replace the emoji below with your own icon image if you want -->
     <div class="splash-icon">
         <img src="image/book.svg" alt="book-icon" style="width: 80%; height: auto;">
     </div>
 
-    <a href="pages/todo.php" class="btn-start">start</a>
+    <a href="todo.php" class="btn-start">start</a>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
